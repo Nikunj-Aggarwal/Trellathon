77b74535ff494d51ebbcdac6fb122fe71d6e594a
utsavshukla0509 <utsavshukla@yahoo.com>
2021-04-24 02:26:30 +0530

    DI Updated

--- trellathon/di/index.js
+++ trellathon/di/index.js
@@ -28,12 +28,14 @@ container.register('getTotalLikesApi', asClass(require("../controller/user/getLi
 container.register('getTotalUploadedVideosApi', asClass(require("../controller/user/getUploadedVideos"), getScope()));
 container.register('getTotalViewsApi', asClass(require("../controller/user/getViews"), getScope()));
 container.register('getTotalSubscribersApi', asClass(require("../controller/user/getSubscribers"), getScope()));
+container.register('getUserTagvideosApi', asClass(require("../controller/videoTag/getVideos"), getScope()));
 
+//UTILITY
 container.register('helper', asClass(require("../utilities/helper"), getScope()));
 
+//REPO
 container.register('userRepo', asClass(require("../repository/userRepo"), getScope()));
-
-
+container.register('videoTagRepo', asClass(require("../repository/videoTagRepo"), getScope()));
 
 
 module.exports = container;
